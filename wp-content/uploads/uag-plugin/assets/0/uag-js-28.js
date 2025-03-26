e573a2df4daa31b0f183a366b1b5e7ddbed8251f
mfbferreira <106761490+mfbferreira@users.noreply.github.com>
2025-03-25 22:02:40 -0400

    Atualização
    Atualização

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-28.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-5d8a0f5e', {"layout":"bars","heading":"Successful Projects","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":90,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-6170d295', {"layout":"bars","heading":"Positive Reviews","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":80,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-37f80cec', {"layout":"bars","heading":"Happy Customers","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":85,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
